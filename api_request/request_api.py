761b538849f98285521988ca0859cc28927c08b0
Alexey Lyutikh <lutixalex1998@gmail.com>
2021-01-14 18:37:31 +0300

    new

--- api_request/request_api.py
+++ api_request/request_api.py
@@ -4,8 +4,8 @@ def get_json(url):
 	response = requests.get(url)
 	return response.json()
 
-def send_json(url, title, description):
-    params = {'title': title, 'description': description}
+def send_json(url, title, description, pic):
+    params = {'title': title, 'description': description, 'photo': open(pic, 'rb')}
     response = requests.post(url, data=params)
     return response
 
@@ -14,6 +14,6 @@ def delete_json(url, id):
 	return response
 
 url = "http://46.101.227.60:8000/api/todo/"
-#send_json(url, 'купить молоко', 'в пяторочке')
+print(send_json(url, 'купить молоко', 'в пяторочке', 'cat.jpg'))
 #delete_json(url)
 print(get_json(url))
